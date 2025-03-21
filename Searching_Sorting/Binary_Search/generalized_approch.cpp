324f7d2dbf558f8df231338acf9d3f0f93345a3b
mashfiqur11 <mashfiqur07@gmail.com>
2025-03-21 17:02:44 +0600

    Update generalized approch

--- Searching_Sorting/Binary_Search/generalized_approch.cpp
+++ Searching_Sorting/Binary_Search/generalized_approch.cpp
@@ -1,34 +1,36 @@
 ///---  In the name of ALLAH    ---///
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
 int arr[] = {0, 0, 0, 0, 1, 1, 1, 1};
-/// Find the last index of last occurance of 0 and first occurance of 1.
 
-
+bool isOk(int ind) /// Change
+{
+    if (arr[ind] == 0) return 1;
+    else return 0;
+}
 
 int32_t main() 
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);cout.tie(NULL);
+    cin.tie(NULL); cout.tie(NULL);
 
-    int n = arr.size();
-    int l = 0, r = n;
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    int l = 0, r = n - 1;
 
-    while (l < r)
+    while (l <= r)
     {
         int mid = (l + r) / 2;
 
-        if (arr[mid] == 0) l = mid + 1;
+        if (isOk(mid)) l = mid + 1;
         else r = mid - 1;
     }
 
-    int ans0 = l-1;
-    int ans1 = l;
+    int ans0 = l - 1; 
+    int ans1 = l;     
 
-    cout << ans0 << " " << ans1 << endl;
+    cout << ans0 << " " << ans1 << endl;    //// Change 
 
     return 0;
 }
